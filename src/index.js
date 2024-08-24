e5cef683083aa3b330fd2d9c1cd95cca4e98da69
Stephen Muniu Kimani <stvnkimani@gmail.com>
2024-08-24 16:50:49 +0300

    made changes to all components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./components/App";
 import "semantic-ui-css/semantic.min.css";
 
+
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container);
